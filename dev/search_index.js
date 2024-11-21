a87c47b67b351646c0c9b92deac9e927c2b5d138
Documenter.jl <documenter@juliadocs.github.io>
2024-11-21 20:14:16 +0000

    build based on c704acf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChromeDevToolsLite","category":"page"},{"location":"#ChromeDevToolsLite","page":"Home","title":"ChromeDevToolsLite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ChromeDevToolsLite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChromeDevToolsLite]","category":"page"}]
+}
